7f5c469291a519b9286bbd75540e013d56b2e94a
Rafael Cabral <rf.cabral00@gmail.com>
2026-01-14 18:06:32 -0300

    docs: aula_4

---
+++ curso-sql/09_distinct.sql
@@ -0,0 +1,7 @@
+-- o distinct do sql vai olhar para todas as colunas da query, 
+-- e ai deduplicar e trazer apenas valores distintos nessa combinação 
+
+
+SELECT COUNT(DISTINCT IdCliente) 
+
+FROM clientes
