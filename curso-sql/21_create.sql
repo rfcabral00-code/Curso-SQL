abe83d3737457c6a4a760ab7d801315f50f931cf
Rafael Cabral <rf.cabral00@gmail.com>
2026-02-03 21:12:01 -0300

    docs: Create

---
+++ curso-sql/21_create.sql
@@ -0,0 +1,26 @@
+--Quantidade de transações Acumuladas ao longo do tempo?
+--quando que a gente atingiu 100k transações (assim que vem a pergunta no trabalho) 
+--para isso, foi adicionado na query o cte de tb_acum e a query abaixo dela
+-- que pegamos o dia que aconteceu de atingir mais de 100k de transacoes acumuladas
+
+WITH tb_diario AS (
+
+    SELECT substr(DtCriacao,1,10) as dtDia,
+            count(DISTINCT IdTransacao) as qtdeTransacao
+
+    FROM transacoes
+
+    GROUP BY dtDia
+
+),
+tb_acum AS (
+        SELECT *,
+                sum(qtdeTransacao) OVER (ORDER BY dtDia) AS qtTransacaoAcum
+        
+        FROM tb_diario
+
+        ORDER BY dtDia
+)
+
+SELECT *
+FROM tb_acum
